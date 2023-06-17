4fa5225f719467810010e1498ba632a69dbfa34d
Kamil Niklasiński <kamil.niklasinski@gmail.com>
2023-06-17 21:17:33 +0100

    Add to lambda code handler and update .gitignore

--- get_s3_buckets.py
+++ get_s3_buckets.py
@@ -1,8 +1,14 @@
 """Example of listing all S3 buckets in an AWS account."""
 
+# pylint: disable=unused-import
+import requests
 import boto3
 
 
-s3 = boto3.resource('s3')
-for bucket in s3.buckets.all():
-    print(bucket.name)
+# pylint: disable=unused-argument
+def lambda_handler(event, context):
+    """Lambda handler."""
+
+    s3_resource = boto3.resource('s3')
+    for bucket in s3_resource.buckets.all():
+        print(bucket.name)
